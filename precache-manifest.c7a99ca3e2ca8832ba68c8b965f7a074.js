311e95b8e0af356889e7123e925372578ce3c224
Artsiom Trafimiuk <algatecby@gmail.com>
2019-07-27 10:50:16 +0300

    Updates

---
+++ precache-manifest.c7a99ca3e2ca8832ba68c8b965f7a074.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86ba92ff997b80176864176b5714ec65",
+    "url": "/ToDo-DevIncubator/index.html"
+  },
+  {
+    "revision": "9a4a7c70ad3b73905376",
+    "url": "/ToDo-DevIncubator/static/css/main.154ff13b.chunk.css"
+  },
+  {
+    "revision": "06f44d7c6eeee966ea35",
+    "url": "/ToDo-DevIncubator/static/js/2.34ad89f7.chunk.js"
+  },
+  {
+    "revision": "9a4a7c70ad3b73905376",
+    "url": "/ToDo-DevIncubator/static/js/main.d4da9fd8.chunk.js"
+  },
+  {
+    "revision": "bee28872e365049f9d2b",
+    "url": "/ToDo-DevIncubator/static/js/runtime~main.dfc886c7.js"
+  }
+]);
